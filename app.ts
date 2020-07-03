d455e068d6624782aa73d82c11c14cab51d048fa
Carlton Joseph <carlton.joseph@gmail.com>
2020-07-03 14:03:20 -0700

    add websocket connection on server and client

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
 import { serve } from "https://deno.land/std/http/server.ts";
+import { acceptWebSocket, acceptable } from "https://deno.land/std/ws/mod.ts";
+
+import { chatConnection } from "./ws/chatroom.ts";
 
 const server = serve({ port: 3000 });
-
 console.log("http://localhost:3000/");
 
 for await (const req of server) {
@@ -12,5 +14,16 @@ for await (const req of server) {
     });
   } else {
     req.respond({ body: "Hi World\n" });
+  }
+
+  if (req.url === "/ws") {
+    if (acceptable(req)) {
+      acceptWebSocket({
+        conn: req.conn,
+        bufReader: req.r,
+        bufWriter: req.w,
+        headers: req.headers,
+      }).then(chatConnection);
+    }
   }
 }
---
+++ ws/chatroom.ts
@@ -0,0 +1,13 @@
+import { WebSocket } from "https://deno.land/std/ws/mod.ts";
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
+
+const sockets = new Map<string, WebSocket>();
+
+const chatConnection = async (ws: WebSocket) => {
+  console.log("new socket connnection");
+  const uid = v4.generate();
+  sockets.set(uid, ws);
+  console.log(sockets);
+};
+
+export { chatConnection };
